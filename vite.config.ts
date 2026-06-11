fc1d168cfb00931ae45b592fc11d48ab0a72d206
nchedochukwugini <nchedochukwuginikachukwu@gmail.com>
2026-06-11 02:54:05 +0100

    Fix Buffer polyfill Vite 8 compatible

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   define: {
     'process.env': {},
     global: 'globalThis',
+    Buffer: ['buffer', 'Buffer'],
   },
   resolve: {
     alias: {
@@ -15,28 +15,10 @@ export default defineConfig({
       buffer: 'buffer',
     },
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-          process: true,
-        }),
-      ],
-    },
-  },
   build: {
     target: 'esnext',
     commonjsOptions: {
       transformMixedEsModules: true,
-    },
-    rolldownOptions: {
-      output: {
-        codeSplitting: true,
-      },
     },
   },
 })
